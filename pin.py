9ed7683f3ed058568dda7cf19aff36734dec4024
CristianVisintin <38670136+CristianVisintin@users.noreply.github.com>
2020-03-04 21:22:48 +0100

    Update pin.py

--- pin.py
+++ pin.py
@@ -3,3 +3,9 @@ from time import sleep
 for i in range(0000, 10000):
     print(f"checkling {i:04}")
     sleep(.1)
+
+with open ("large", "r") as file:
+    for word in file.readlines():
+        print(f"Checking {word.rstrip()}...")
+        sleep(.1)
+       
